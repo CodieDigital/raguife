62cd7d53b34bf3685fa4b2bc64499ef74d069526
Your Name <you@example.com>
2022-09-02 15:11:30 -0300

    feat: ajuste bug

--- pages/quem-somos.tsx
+++ pages/quem-somos.tsx
@@ -15,9 +15,9 @@ interface InformationPageProps {
   information: PageInformationProps;
 }
 
-const router = useRouter()
-
 export default function SobrePage({ information }: InformationPageProps) {
+  const router = useRouter();
+
   return (
     <Layout>
       <Head>
@@ -26,28 +26,37 @@ export default function SobrePage({ information }: InformationPageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="description" content={information.head.metaDescription} />
-        <meta itemProp="description" content={information.head.metaDescription} />
-        <meta name="twitter:description" content={information.head.metaDescription} />
-        <meta name="og:description" content={information.head.metaDescription} />
-        <meta property="og:title" content={information.head.pageTitle}/>
-        <meta property="og:locale" content="pt_BR"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content={router.query.pathname?.toString()}/>
-        <meta property="og:image" content={information.head.imageOpenGraph}/>
+        <meta
+          itemProp="description"
+          content={information.head.metaDescription}
+        />
+        <meta
+          name="twitter:description"
+          content={information.head.metaDescription}
+        />
+        <meta
+          name="og:description"
+          content={information.head.metaDescription}
+        />
+        <meta property="og:title" content={information.head.pageTitle} />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={router.query.pathname?.toString()} />
+        <meta property="og:image" content={information.head.imageOpenGraph} />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="content-language" content="pt-br"/>
-        <meta name="rating" content="general"/>
-        <meta name="distribution" content="global"/>
-        <meta name="copyright" content="Codie"/>
+        <meta name="content-language" content="pt-br" />
+        <meta name="rating" content="general" />
+        <meta name="distribution" content="global" />
+        <meta name="copyright" content="Codie" />
         <meta name="author" content="Codie" />
         <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index,follow"/>
+        <meta name="googlebot" content="index,follow" />
         <link rel="canonical" href={router.pathname} />
         <meta name="referrer" content="origin" />
-        <meta name="language" content="portuguese, PT"/>
-        <meta name="location" content="Londrina, Paraná"/>
+        <meta name="language" content="portuguese, PT" />
+        <meta name="location" content="Londrina, Paraná" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="keyboard-shortcuts-preference" content="all"/>
+        <meta name="keyboard-shortcuts-preference" content="all" />
       </Head>
 
       <S.Sobre>
